50e9b36a8577cf3618098ef1b72dc3465db54bbc
Chanchal <chanchal@naxtre.com>
2021-01-14 18:51:13 +0530

    first commit

---
+++ testFramwork/testFramwork.h
@@ -0,0 +1,18 @@
+//
+//  testFramwork.h
+//  testFramwork
+//
+//  Created by IOS on 12/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testFramwork.
+FOUNDATION_EXPORT double testFramworkVersionNumber;
+
+//! Project version string for testFramwork.
+FOUNDATION_EXPORT const unsigned char testFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testFramwork/PublicHeader.h>
+
+
